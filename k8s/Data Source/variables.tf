f800e9a920feb15f0f522c4b3509ec0c34d3263d
SundarK <88338291+sundar-k565@users.noreply.github.com>
2025-06-03 02:47:46 +0530

    Create variables.tf

---
+++ k8s/Data Source/variables.tf
@@ -0,0 +1,22 @@
+provider "e2e" {
+  api_key    = var.api_key_prod
+  auth_token = var.auth_token_prod
+}
+
+variable "api_key_prod" {
+  type = string
+  sensitive = true
+}
+
+variable "auth_token_prod" {
+  type = string
+  sensitive = true
+}
+
+variable "kubernetes_name" {
+  type = string
+}
+
+variable "project_id" {
+  type = number
+}
