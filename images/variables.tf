8bca3bab7afa8d75397dd56ee6ba3713ab1ff3d9
SundarK <88338291+sundar-k565@users.noreply.github.com>
2025-06-03 02:34:48 +0530

    Create variables.tf

---
+++ images/variables.tf
@@ -0,0 +1,30 @@
+provider "e2e" {
+  api_key    = var.api_key_prod
+  auth_token = var.auth_token_prod
+}
+
+variable "api_key_prod" {
+  type      = string
+  sensitive = true
+}
+
+variable "auth_token_prod" {
+  type      = string
+  sensitive = true
+}
+
+variable "image_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
